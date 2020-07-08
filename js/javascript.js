dd21835899665841dd732ca20e91e4fc66caf953
Gladys Cruz <cruzgladysazucena@johndeere.com>
2020-07-08 09:25:11 -0500

    Creating grid 3x3

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+const div1Btn = document.querySelector("#div1");
+const div2Btn = document.querySelector("#div2");
+const div3Btn = document.querySelector("#div3");
+const div4Btn = document.querySelector("#div4");
+const div5Btn = document.querySelector("#div5");
+const div6Btn = document.querySelector("#div6");
+const div7Btn = document.querySelector("#div7");
+const div8Btn = document.querySelector("#div8");
+const div9Btn = document.querySelector("#div9");
+
+
+div1Btn.addEventListener('click', changeColor);
+div2Btn.addEventListener('click', changeColor);
+div3Btn.addEventListener('click', changeColor);
+div4Btn.addEventListener('click', changeColor);
+div5Btn.addEventListener('click', changeColor);
+div6Btn.addEventListener('click', changeColor);
+div7Btn.addEventListener('click', changeColor);
+div8Btn.addEventListener('click', changeColor);
+div9Btn.addEventListener('click', changeColor);
+
+
+function changeColor() {
+    console.log("change color");
+}
