583c2b88951f1123506ede98cb2eb88e92ad0743
Gladys Cruz <cruzgladysazucena@johndeere.com>
2020-07-09 00:31:51 -0500

    game board

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,12 @@ let playerTurn = true;
 let divAllBtn = document.querySelectorAll(".grid-item");
 let resetBtn = document.querySelector(".button");
 let counterMoves = 0;
+let bluePlayerScore = 0;
+let redPlayerScore = 0;
 let winnerCombinations = ["012", "345", "678", "258", "147", "036", "246", "048"];
+let divBlueScore = document.querySelector("#blue-score");
+let divRedScore = document.querySelector("#red-score");
+
 
 console.log(divAllBtn);
 let changeColor = (event) => {
@@ -12,6 +17,7 @@ let changeColor = (event) => {
 
     if (event.target.style.background !== "") {
         alert("Please select diferent square, square has been clicked and you should not be able to click it again");
+        counterMoves--;
     } else {
         if (playerTurn) {
             event.target.style.background = "red";
@@ -46,45 +52,47 @@ resetBtn.addEventListener('click', resetColor);
 
 
 function validateWinner() {
-    console.log("Validate winner: divAllBtn[0] background " + divAllBtn[0].style.background);
-    console.log("Validate winner: divAllBtn[1] background " + divAllBtn[1].style.background);
-    console.log("Validate winner: divAllBtn[2] background " + divAllBtn[2].style.background);
-    console.log("Validate winner: divAllBtn[3] background " + divAllBtn[3].style.background);
-    console.log("Validate winner: divAllBtn[4] background " + divAllBtn[4].style.background);
-    console.log("Validate winner: divAllBtn[5] background " + divAllBtn[5].style.background);
-    console.log("Validate winner: divAllBtn[6] background " + divAllBtn[6].style.background);
-    console.log("Validate winner: divAllBtn[7] background " + divAllBtn[7].style.background);
-    console.log("Validate winner: divAllBtn[8] background " + divAllBtn[8].style.background);
 
-    if (divAllBtn[0].style.background === divAllBtn[1].style.background && divAllBtn[0].style.background === divAllBtn[2].style.background && divAllBtn[0].style.background !== "") {
-        console.log(`1 Winner is ${divAllBtn[0].style.background} player`);
+    let displayMessage = "";
+    let winPlayer = "";
+
+    if ((divAllBtn[0].style.background === divAllBtn[1].style.background && divAllBtn[0].style.background === divAllBtn[2].style.background && divAllBtn[0].style.background !== "") ||
+        (divAllBtn[0].style.background === divAllBtn[4].style.background && divAllBtn[0].style.background === divAllBtn[8].style.background && divAllBtn[0].style.background !== "") ||
+        (divAllBtn[0].style.background === divAllBtn[3].style.background && divAllBtn[0].style.background === divAllBtn[6].style.background && divAllBtn[0].style.background !== "")) {
+        winPlayer = divAllBtn[0].style.background;
+        console.log(winPlayer);
     }
-    else if (divAllBtn[3].style.background === divAllBtn[4].style.background && divAllBtn[3].style.background === divAllBtn[5].style.background && divAllBtn[3].style.background !== "") {
-        console.log(`2 Winner is ${divAllBtn[3].style.background} player`);
+    else if ((divAllBtn[3].style.background === divAllBtn[4].style.background && divAllBtn[3].style.background === divAllBtn[5].style.background && divAllBtn[3].style.background !== "") ||
+        (divAllBtn[2].style.background === divAllBtn[4].style.background && divAllBtn[2].style.background === divAllBtn[6].style.background && divAllBtn[2].style.background !== "") ||
+        (divAllBtn[1].style.background === divAllBtn[4].style.background && divAllBtn[1].style.background === divAllBtn[7].style.background && divAllBtn[1].style.background !== "")) {
+        winPlayer = divAllBtn[4].style.background;
+        console.log(winPlayer);
+    }
+    else if ((divAllBtn[6].style.background === divAllBtn[7].style.background && divAllBtn[6].style.background === divAllBtn[8].style.background && divAllBtn[6].style.background !== "") ||
+        (divAllBtn[2].style.background === divAllBtn[5].style.background && divAllBtn[2].style.background === divAllBtn[8].style.background && divAllBtn[2].style.background !== "")) {
+        winPlayer = divAllBtn[8].style.background;
+        console.log(winPlayer);
 
     }
-    else if (divAllBtn[6].style.background === divAllBtn[7].style.background && divAllBtn[6].style.background === divAllBtn[8].style.background && divAllBtn[6].style.background !== "") {
-        console.log(`3 Winner is ${divAllBtn[6].style.background} player`);
+    else {
+        if (counterMoves === 9 && winPlayer === "") {
+            console.log("9 No winner yet");
+        }
+        else if (winPlayer !== "") {
+            console.log("No winner!!");
+        }
     }
-    else if (divAllBtn[2].style.background === divAllBtn[5].style.background && divAllBtn[2].style.background === divAllBtn[8].style.background && divAllBtn[2].style.background !== "") {
-        console.log(` 4 Winner is ${divAllBtn[2].style.background} player`);
-    }
-    else if (divAllBtn[1].style.background === divAllBtn[4].style.background && divAllBtn[1].style.background === divAllBtn[7].style.background && divAllBtn[1].style.background !== "") {
-        console.log(`5 Winner is ${divAllBtn[1].style.background} player`);
-    }
-    else if (divAllBtn[0].style.background === divAllBtn[3].style.background && divAllBtn[0].style.background === divAllBtn[6].style.background && divAllBtn[0].style.background !== "") {
-        console.log(`6 Winner is ${divAllBtn[0].style.background} player`);
-    }
-    else if (divAllBtn[2].style.background === divAllBtn[4].style.background && divAllBtn[2].style.background === divAllBtn[6].style.background && divAllBtn[2].style.background !== "") {
-        console.log(`7 Winner is ${divAllBtn[2].style.background} player`);
-    }
-    else if (divAllBtn[0].style.background === divAllBtn[4].style.background && divAllBtn[0].style.background === divAllBtn[8].style.background && divAllBtn[0].style.background !== "") {
-        console.log(`8 Winner is ${divAllBtn[0].style.background} player`);
-    }
-    else if (counterMoves === 9) {
-        console.log("9 No winner yet");
-    } else {
-        console.log("No winner!!");
+
+    if (winPlayer !== "") {
+        displayMessage = `Winner is ${winPlayer} player`;
+        if (winPlayer === "blue") {
+            bluePlayerScore++;
+            divBlueScore.innerHTML = bluePlayerScore;
+        } else {
+            redPlayerScore++;
+            divRedScore.innerHTML = redPlayerScore;
+        }
+        console.log(displayMessage);
     }
 
 }
